30f27d39c9b495ae5658aef36d216c07826aa074
Tsinswreng <Tsinswreng@gmail.com>
2025-10-23 19:56:29 +0800

    init

---
+++ proj/Tsinswreng.CsFactoryMkr/MkFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace Tsinswreng.CsFactoryMkr;
+
+[AttributeUsage(
+	AttributeTargets.Class | AttributeTargets.Struct
+	, AllowMultiple = false
+	, Inherited = false
+)]
+public sealed class MkFactory : Attribute {
+	public Type For { get; set; }   // 保留，未來可擴充
+	public Type Source { get; set; }   // 保留
+	public string MethodName { get; set; } = "Mk";
+	public string[] IgnoredFields { get; set; } = Array.Empty<string>();
+	public MkFactory(){
+		if(Source is null){
+			Source = For;
+		}
+	}
+}
